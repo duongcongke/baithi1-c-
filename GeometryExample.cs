a476dcbca0eda3cbe48e031b899022b4fa78c489
duongcongke <103102773+duongcongke@users.noreply.github.com>
2022-08-06 17:18:59 +0700

    up

---
+++ GeometryExample.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace calc
+{
+    class CalcForula
+    {
+        public static void Main1(string[] args)
+        {
+            const double pi = 3.14159;
+            int radius;
+            int height;
+            double BaseArea;
+            double LateralArea;
+            double TotalArea;
+            double Volume;
+            Console.WriteLine("Enter radius: ");
+            radius = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter height: ");
+            height = Convert.ToInt32(Console.ReadLine());
+            BaseArea = radius * radius * pi;
+            LateralArea = 2 * pi * radius * height;
+            TotalArea = 2 * pi * radius * (height + radius);
+            Volume = pi * radius * radius * height;
+            Console.WriteLine("Cylender Characteristics");
+            Console.Write("Radius :" + radius);
+            Console.WriteLine(", Height :" + height);
+            Console.Write("Base :" + BaseArea);
+            Console.Write(" | Lateral :" + LateralArea);
+            Console.Write(" | Total :" + TotalArea);
+            Console.Write(" | Volume :" + Volume);
+
+
+        }
+    }
+}
---
+++ Inherittance_Polymorphism.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace demoInher_Poly
+{
+
+
+    class show
+    {
+        public string name;
+        public int weight;
+        public show()
+        {
+            this.name = name;
+            this.weight = weight;
+        }
+    }
+    class Lion : show
+    {
+        // public Lion(){
+
+        // }
+    }
+    class Tiger : show
+    {
+
+    }
+    public class testproduct
+    {
+        public static void Main(string[] args)
+        {
+            Lion lion1 = new Lion();
+            lion1.name = "Lion";
+            lion1.weight = 100;
+            Console.Write("Name : " + lion1.name);
+            Console.WriteLine(", Weight : " + lion1.weight);
+            Tiger tiger1 = new Tiger();
+            tiger1.name = "Tiger";
+            tiger1.weight = 200;
+            Console.Write("Name : " + tiger1.name);
+            Console.WriteLine(", Weight : " + tiger1.weight);
+        }
+    }
+}
